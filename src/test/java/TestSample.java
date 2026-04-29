a11f352b7dc5d6a181172fadc616ec74e2d8ef26
Madhavan <madhavan86776@gmail.com>
2026-04-29 09:39:39 +0530

    Add unit test for addition in TestSample

---
+++ src/test/java/TestSample.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSample {
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
